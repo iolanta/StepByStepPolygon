8354a39a5cee66c22aeb080a619d4ff6fd4a6393
unknown <iolanta.paly@gmail.com>
2018-10-22 20:33:53 +0300

    online polygone done

--- OnlinePolygon/Form1.Designer.cs
+++ OnlinePolygon/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(906, 559);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(931, 630);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "OnlinePolygon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- OnlinePolygon/Form1.cs
+++ OnlinePolygon/Form1.cs
@@ -12,9 +12,149 @@ namespace OnlinePolygon
 {
     public partial class Form1 : Form
     {
+        private List<PointF> polygon_points;
         public Form1()
         {
             InitializeComponent();
+            polygon_points = new List<PointF>();
+        }
+
+        //  Считает, пересекаются ли отрезки (p1,p2) и (p3,p4). 
+        private bool FindIntersection(PointF p1, PointF p2, PointF p3, PointF p4, out PointF intersection)
+        {
+            float dx12 = p2.X - p1.X;
+            float dy12 = p2.Y - p1.Y;
+            float dx34 = p4.X - p3.X;
+            float dy34 = p4.Y - p3.Y;
+
+            float denominator = (dy12 * dx34 - dx12 * dy34);
+
+            float t1 = ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34) / denominator;
+            if (float.IsInfinity(t1))
+            {
+                intersection = new PointF(float.NaN, float.NaN);
+                return false;
+            }
+                
+            float t2 = ((p3.X - p1.X) * dy12 + (p1.Y - p3.Y) * dx12) / -denominator;
+
+            intersection = new PointF(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+
+            return ((t1 >= 0) && (t1 <= 1) && (t2 >= 0) && (t2 <= 1));
+        }
+
+        private bool point_in_polygon(PointF p)
+        {
+            for (int i = 0; i < polygon_points.Count; ++i)
+                if (polygon_points[i] == p)
+                    return true;
+
+
+            int cnt_intersec = 0;
+            PointF intersec;
+            PointF finish = new PointF(0, 0);
+            for (var i = 0; i < polygon_points.Count - 1; ++i)
+            {
+                if (FindIntersection(p, finish, polygon_points[i], polygon_points[i + 1], out intersec) && intersec != polygon_points[i])
+                    ++cnt_intersec;
+            }
+
+            if (FindIntersection(p, finish, polygon_points.First(), polygon_points.Last(), out intersec) && intersec != polygon_points.First())
+                ++cnt_intersec;
+
+            if (cnt_intersec % 2 != 0)     // принадлежит многоугольнику
+                return true;
+            else                           // не принадлежит
+                return false;   
+        }
+
+        // Расстояние между двумя точками
+        float distance(PointF p1, PointF p2)
+        {
+            return (float)Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
+
+        int orientation(PointF a, PointF b, PointF c)
+        {
+            float res = (b.Y - a.Y) * (c.X - b.X) - (c.Y - b.Y) * (b.X - a.X);
+
+            if (res == 0)
+                return 0;
+            if (res > 0)
+                return 1;
+            return -1;
+        }
+
+        private void add_new_point(PointF p)
+        {
+            if (point_in_polygon(p))
+                return;
+
+            // поиск точки, имеющей минимальное расстояние до p
+            int ind = 0;
+            int n = polygon_points.Count;
+            for (int i = 0; i < n; ++i)
+            {
+                if (distance(p, polygon_points[i]) < distance(p, polygon_points[ind]))
+                    ind = i;
+            }
+
+            // поиск касательных
+            int t1 = ind;
+            while(orientation(p, polygon_points[t1], polygon_points[(t1 + 1) % n]) >= 0)
+                t1 = (t1 + 1) % n;
+
+            int t2 = ind;
+            while (orientation(p, polygon_points[t2], polygon_points[(n + t2 - 1) % n]) <= 0)
+                t2 = (n + t2 - 1) % n;
+
+            // создание нового полигона
+            List<PointF> new_polygon = new List<PointF>();
+            int cur = t1;
+            new_polygon.Add(polygon_points[cur]);
+            while(cur != t2)
+            {
+                cur = (cur + 1) % n;
+                new_polygon.Add(polygon_points[cur]);
+            }
+
+            new_polygon.Add(p);
+            polygon_points = new_polygon;
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (polygon_points.Count <= 2)
+                polygon_points.Add(e.Location);
+            else
+                add_new_point(e.Location);
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pnt = new Pen(Color.Fuchsia, 2);
+            Pen ln = new Pen(Color.Blue);
+            if (polygon_points.Count == 1)
+                e.Graphics.DrawEllipse(pnt, polygon_points[0].X, polygon_points[0].Y, 2, 2);
+            else if(polygon_points.Count >= 2)
+            {
+                for(int i = 0; i < polygon_points.Count - 1; ++i)
+                {
+                    e.Graphics.DrawEllipse(pnt, polygon_points[i].X, polygon_points[i].Y, 2, 2);
+                    e.Graphics.DrawLine(ln, polygon_points[i], polygon_points[i + 1]);
+                }
+                e.Graphics.DrawLine(ln, polygon_points.Last(), polygon_points.First());
+                e.Graphics.DrawEllipse(pnt, polygon_points.Last().X, polygon_points.Last().Y, 2, 2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            polygon_points.Clear();
+            pictureBox1.Invalidate();
         }
     }
 }
